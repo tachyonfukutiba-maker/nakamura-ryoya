41689cb67ddb4425719f5e29cf9ae0329f623456
nakamura ryoya <tachyonfukutiba@gmail.com>
2026-07-07 00:04:21 +0900

    chore: GitHub Pages用のデプロイ設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // 🌟こちらに書き換えます
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/nakamura-ryoya/', // 👈ここはリポジトリ名のままでOK！
 })
